bac77b9c02c082ea337132af6171e2df02f06a21
PawanachaiSeeruesang <kanpim5@gmail.com>
2019-02-11 16:43:30 +0700

    two commit

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,12 +1,56 @@
+// let express = require("express");
+// const app = express();
+
+// app.get("/status", function(req, res) {
+//   res.send("Hello nodejs server");
+// });
+
+// let port = 8080;
+
+// app.listen(port, function() {
+//   console.log("server running on" + port);
+// });
+// let express = require("express");
+
+// const app = express();
+
+// app.get("/status", function(req, res) {
+//   res.send("Hello nodejs server");
+// });
+
+// app.get("/hello/:person", function(req, res) {
+//   res.send("sey hello with " + req.params.person);
+// });
+
+// let port = 8080;
+
+// app.listen(port, function() {
+//   console.log("server running on " + port);
+// });
+
 let express = require("express");
+let bodyParser = require("body-parser");
+
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/status", function(req, res) {
   res.send("Hello nodejs server");
 });
 
+app.get("/hello/:person", function(req, res) {
+  console.log("hello - " + req.params.person);
+  res.send("sey hello with " + req.params.person);
+});
+
+app.post("/hello", function(req, res) {
+  res.send("OK  you post - " + req.body.name);
+});
+
 let port = 8080;
 
 app.listen(port, function() {
-  console.log("server running on" + port);
+  console.log("server running on " + port);
 });
